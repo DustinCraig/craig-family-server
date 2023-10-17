a989070f4b8fb974965d75bbcd22bdf5dbe7295e
Dustin Craig <dustincraig@MacBook-Pro-2.attlocal.net>
2023-10-17 19:05:06 -0400

    init

---
+++ src/lib/parse-weather.js
@@ -0,0 +1,38 @@
+const convertEpochToTime = (epoch) => {
+  const date = new Date(epoch);
+  return date.toLocaleTimeString();
+};
+
+const parseDay = (day) => {
+  const { temp, sunrise, sunset, weather } = day;
+  let parsedTemp = typeof temp === "object" ? temp.day : temp;
+  return {
+    temp: parsedTemp,
+    sunrise: convertEpochToTime(sunrise * 1000),
+    sunset: convertEpochToTime(sunset * 1000),
+    weather: weather.length ? weather[0].description : "",
+  };
+};
+
+export function parseWeather(weather) {
+  const { current, daily } = weather;
+  const currentWeather = parseDay(current);
+  const tomorrow = parseDay(daily[0]);
+
+  return {
+    current: currentWeather,
+    tomorrow,
+  };
+}
+
+export function weatherToText(parsedWeather) {
+  let result = `Craig Family Weather Report:`;
+  const { current, tomorrow } = parsedWeather;
+
+  result += "\n";
+  result += `The current weather is ${current.weather} and the temperature is: ${current.temp}`;
+  result += "\n";
+  result += `Tomorrow, the weather will be ${tomorrow.weather} and the temperature will be: ${tomorrow.temp}`;
+  result += "\nHave a great day!";
+  return result;
+}
---
+++ src/server.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import twilio from "twilio";
+import { parseWeather, weatherToText } from "./lib/parse-weather.js";
+
+const app = express();
+const port = 3000;
+
+const weatherApi = process.env.WEATHER_API;
+const twilioAuth = process.env.TWILIO_AUTH;
+const twilioSID = process.env.TWILIO_SID;
+const twilioPN = process.env.TWILIO_PN;
+const numberList = process.env.NUMBER_LIST;
+const twilioClient = twilio(twilioSID, twilioAuth);
+
+app.get("/healthcheck", async (req, res) => {
+  res.send("OK");
+  const weatherResponse = await fetch(
+    `http://api.openweathermap.org/data/3.0/onecall?lat=${35.9606}&lon=${-83.926453}&appid=${weatherApi}&exclude=minutely,hourly&units=imperial`
+  );
+  const weather = await weatherResponse.json();
+  const numbers = numberList.split(";");
+  numbers.map((num) => {
+    twilioClient.messages
+      .create({
+        from: twilioPN,
+        to: num,
+        body: `${weatherToText(parseWeather(weather))}`,
+      })
+      .then((_) => {
+        console.log("message sent");
+      })
+      .catch((err) => {
+        console.log("err ", err);
+      });
+  });
+});
+
+app.get("/weather", async (req, res) => {
+  try {
+    res.send(weather);
+  } catch (err) {
+    res.status(500).send({
+      err: "An error occurred while attempting to retrieve weather",
+      success: false,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.info(`App started on port: ${port}`);
+});
